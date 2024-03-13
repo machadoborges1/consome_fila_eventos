1ea6e84164fa27140ed3042c4b2983fae0c585d5
Humberto Neto <humberto.neto@unitri.edu.br>
2024-03-13 11:15:48 -0300

    processa btd fat gr

--- internal/service/processa_bdt_fat_gr.go
+++ internal/service/processa_bdt_fat_gr.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	"github.com/machadoborges1/consome_fila_eventos/internal/entity"
+	go_ora "github.com/sijms/go-ora/v2"
 )
 
 func ProcessaBDTFatGR(db *sql.DB, evento entity.TCBContrFilaEventos) {
@@ -15,11 +16,9 @@ func ProcessaBDTFatGR(db *sql.DB, evento entity.TCBContrFilaEventos) {
 
 	fmt.Println("Processando evento tipo BDT_FAT_GR")
 
-	// Defindo a transação para que possa ser revertida em caso de erro
 	tx, err := db.Begin()
 	if err != nil {
 		fmt.Println("Erro ao iniciar a transação:", err)
-		// Trate o erro de acordo com o seu caso, por exemplo, registrando-o, lançando um panic ou retornando
 		return
 	}
 
@@ -35,11 +34,17 @@ func ProcessaBDTFatGR(db *sql.DB, evento entity.TCBContrFilaEventos) {
 		log.Fatal(err)
 	}
 
-	_, err = db.Exec("BEGIN "+
-		"PCB_GERA_PREVISTO_MOV(pDt_Base => :1, pCod_Periodo => :2, pCod_Pessoa => :3, "+
-		"pCod_Fip_GF => :4, pCod_Grupo_Fin => :5, pCod_Servico => :6, pCod_Parcela => :7, "+
-		"pNecessita_Existir => :8, pNro_Sequencial => :9, pMens_Erro => :10); "+
-		"END;",
+	stmt, err := db.Prepare("BEGIN " +
+		"PCB_GERA_PREVISTO_MOV(pDt_Base => :1, pCod_Periodo => :2, pCod_Pessoa => :3, " +
+		"pCod_Fip_GF => :4, pCod_Grupo_Fin => :5, pCod_Servico => :6, pCod_Parcela => :7, " +
+		"pNecessita_Existir => :8, pNro_Sequencial => :9, pMens_Erro => :10); " +
+		"END;")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(
 		vDt_Atual,
 		evento.CodPeriodo.String,
 		evento.CodPessoa.Int64,
@@ -48,38 +53,35 @@ func ProcessaBDTFatGR(db *sql.DB, evento entity.TCBContrFilaEventos) {
 		evento.CodServico.Int64,
 		evento.CodParcela.String,
 		"N",
-		vNroSequencial,
-		vMensErro)
+		sql.Out{Dest: &vNroSequencial},
+		go_ora.Out{Dest: &vMensErro, Size: 200})
 	if err != nil {
-		fmt.Println("erro1")
 		log.Fatal(err)
 	} else {
-		fmt.Printf("okkkkkk")
+		fmt.Println(vNroSequencial, vMensErro)
 	}
 
-	if vMensErro != "" {
+	if vMensErro == "" {
 		if vNroSequencial == 0 {
-			fmt.Println("erro2")
+			fmt.Println("Devolveu Nro_Seq_Fat nulo")
 			tx.Rollback()
-			fmt.Println("numero sequencial nulo")
-		}
-		_, err := db.Exec("BEGIN FIT.PF_SET_CONTABILIZADO_FICHA_FIN("+
-			":pCod_Periodo, :pCod_Pessoa, :pCod_Fip, :pCod_Grupo_Fin, :pCod_Servico, :pCod_Parcela); END;",
-			evento.CodPeriodo, evento.CodPessoa, evento.CodFipGf, evento.CodGrupoFin, evento.CodServico, evento.CodParcela)
-		if err != nil {
-			fmt.Println("erro3")
-			log.Println(err)
 		} else {
-			fmt.Println("erro4")
-			tx.Commit()
+			_, err := db.Exec("BEGIN FIT.PF_SET_CONTABILIZADO_FICHA_FIN("+
+				":pCod_Periodo, :pCod_Pessoa, :pCod_Fip, :pCod_Grupo_Fin, :pCod_Servico, :pCod_Parcela); END;",
+				evento.CodPeriodo, evento.CodPessoa, evento.CodFipGf, evento.CodGrupoFin, evento.CodServico, evento.CodParcela)
+			if err != nil {
+				log.Println(err)
+			} else {
+				fmt.Println("commitado")
+				tx.Commit()
+			}
 		}
 	} else {
-		fmt.Println("erro5")
+		fmt.Println("Existe vMensErro")
 		tx.Rollback()
 	}
 
 	if vMensErro == "" {
-		fmt.Println("erro6")
 		_, err := db.Exec(`DELETE FROM TCB_CONTR_FILA_EVENTOS
 			WHERE STATUS IN ('P','E')
 			AND TIPO_ACAO = :1
@@ -94,13 +96,18 @@ func ProcessaBDTFatGR(db *sql.DB, evento entity.TCBContrFilaEventos) {
 		if err != nil {
 			fmt.Println("erro7")
 			log.Fatal(err)
+		} else {
+			fmt.Println("Deletado")
 		}
+
 		_, err = db.Exec(`INSERT INTO TCB_CONTR_FILA_EVENTOS
 		(ID_EVENTO, TIPO_ACAO, STATUS, DT_BASE, NRO_SEQ_FAT)
 		VALUES (:1, 'LOG_FAT', 'A', :2, :3)`, fsGetIdAlt, evento.DtBase, vNroSequencial)
 		if err != nil {
 			fmt.Println("erro8")
 			log.Fatal(err)
+		} else {
+			fmt.Println("inserido na tabela")
 		}
 
 	} else {
@@ -123,82 +130,3 @@ func ProcessaBDTFatGR(db *sql.DB, evento entity.TCBContrFilaEventos) {
 		}
 	}
 }
-
-// func processaBDTFatGR(db *sql.DB, evento *entity.TCBContrFilaEventos) error {
-// 	var vMensErro string
-// 	var vNroSequencial int64
-
-// 	fmt.Println("Processando evento tipo BDT_FAT_GR")
-
-// 	// Defina a transação para que possa ser revertida em caso de erro
-// 	tx, err := db.Begin()
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// Chame a função PCB_GERA_PREVISTO_MOV
-// 	err = pcbGeraPrevistoMov(db, evento)
-// 	if err != nil {
-// 		tx.Rollback() // Reverta a transação em caso de erro
-// 		return err
-// 	}
-
-// 	if vMensErro == "" {
-// 		if vNroSequencial == 0 {
-// 			tx.Rollback()
-// 			return fmt.Errorf("Devolveu Nro_Seq_Fat nulo")
-// 		}
-
-// 		// Chame a função FIT.PF_SET_CONTABILIZADO_FICHA_FIN
-// 		err = fitPfSetContabilizadoFichaFin(db, evento)
-// 		if err != nil {
-// 			tx.Rollback()
-// 			return err
-// 		}
-
-// 		tx.Commit() // Commit se não houver erros
-// 	} else {
-// 		tx.Rollback() // Reverta a transação em caso de erro
-// 	}
-
-// 	// Se não houver erros, execute a exclusão e inserção conforme necessário
-// 	if vMensErro == "" {
-// 		_, err := db.Exec(`DELETE FROM TCB_CONTR_FILA_EVENTOS
-// 			WHERE STATUS IN ('P','E')
-// 			AND TIPO_ACAO = ?
-// 			AND ID_EVENTO <= ?
-// 			AND COD_PERIODO = ?
-// 			AND COD_PESSOA = ?
-// 			AND COD_FIP_GF = ?
-// 			AND COD_GRUPO_FIN = ?
-// 			AND COD_SERVICO = ?
-// 			AND COD_PARCELA = ?`, evento.TipoAcao, evento.IDEvento, evento.CodPeriodo, evento.CodPessoa, evento.CodFipGf, evento.CodGrupoFin, evento.CodServico, evento.CodParcela)
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		_, err = db.Exec(`INSERT INTO TCB_CONTR_FILA_EVENTOS
-// 			(ID_EVENTO, TIPO_ACAO, STATUS, DT_BASE, NRO_SEQ_FAT)
-// 			VALUES (?, 'LOG_FAT', 'A', ?, ?)`, fsGetIdAlt, evento.DtBase, vNroSequencial)
-// 		if err != nil {
-// 			return err
-// 		}
-// 	} else {
-// 		_, err := db.Exec(`UPDATE TCB_CONTR_FILA_EVENTOS
-// 			SET STATUS = 'E', MENS_ERRO = ?
-// 			WHERE STATUS = 'P'
-// 			AND TIPO_ACAO = ?
-// 			AND ID_EVENTO <= ?
-// 			AND COD_PERIODO = ?
-// 			AND COD_PESSOA = ?
-// 			AND COD_FIP_GF = ?
-// 			AND COD_GRUPO_FIN = ?
-// 			AND COD_SERVICO = ?
-// 			AND COD_PARCELA = ?`, vMensErro, evento.TipoAcao, evento.IDEvento, evento.CodPeriodo, evento.CodPessoa, evento.CodFipGf, evento.CodGrupoFin, evento.CodServico, evento.CodParcela)
-// 		if err != nil {
-// 			return err
-// 		}
-// 	}
-
-// 	return nil
-// }
